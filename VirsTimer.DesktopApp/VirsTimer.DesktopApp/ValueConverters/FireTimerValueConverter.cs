3ffbbfebe249515b6a5880f4016d2e28327a8fc8
Bartosz Hejduk <barhej@st.amu.edu.pl>
2021-04-26 21:18:03 +0200

    Cleaned FireTimerValueConverter class

--- VirsTimer.DesktopApp/VirsTimer.DesktopApp/ValueConverters/FireTimerValueConverter.cs
+++ VirsTimer.DesktopApp/VirsTimer.DesktopApp/ValueConverters/FireTimerValueConverter.cs
@@ -8,19 +8,6 @@ namespace VirsTimer.DesktopApp.ValueConverters
 {
     public class FireTimerValueConverter : IValueConverter
     {
-        //public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        //{
-        //    throw new NotImplementedException();
-        //    if (!(value is DelayFireTimer delayFireTimer))
-        //        return Brushes.DarkGray;
-        //    if (delayFireTimer.CanFire)
-        //        return Brushes.GreenYellow;
-        //    if (!delayFireTimer.CanFire && delayFireTimer.IsRunning)
-        //        return Brushes.MediumVioletRed;
-
-        //    return Brushes.DarkGray;
-        //}
-
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (!(value is DelayStopwatchTimer delayFireTimer))
