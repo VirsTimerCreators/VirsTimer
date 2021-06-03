a4c721b6dd7e9d808d55cb8838668ced314595ee
Bartosz Hejduk <barhej@st.amu.edu.pl>
2021-06-03 12:21:27 +0200

    Added summary in Event model.

--- VirsTimer.DesktopApp/VirsTimer.Core/Models/Event.cs
+++ VirsTimer.DesktopApp/VirsTimer.Core/Models/Event.cs
@@ -1,9 +1,18 @@
 ﻿namespace VirsTimer.Core.Models
 {
+    /// <summary>
+    /// Model representing cube event.
+    /// </summary>
     public class Event
     {
+        /// <summary>
+        /// Event name.
+        /// </summary>
         public string Name { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Event"/> class.
+        /// </summary>
         public Event(string name)
         {
             Name = name;
