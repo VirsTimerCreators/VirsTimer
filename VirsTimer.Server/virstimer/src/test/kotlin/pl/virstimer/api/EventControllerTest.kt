8ea8cb97d997ab1df40a5344742c827010651611
Javorus <40281528+Javorus@users.noreply.github.com>
2021-06-05 13:46:25 +0200

    all events for user

--- VirsTimer.Server/virstimer/src/test/kotlin/pl/virstimer/api/EventControllerTest.kt
+++ VirsTimer.Server/virstimer/src/test/kotlin/pl/virstimer/api/EventControllerTest.kt
@@ -1,13 +1,13 @@
 package pl.virstimer.api
 
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import pl.virstimer.TestCommons
 import pl.virstimer.db.security.model.User
@@ -15,11 +15,11 @@ import pl.virstimer.db.security.model.User
 @SpringBootTest
 @ExtendWith(SpringExtension::class)
 @AutoConfigureMockMvc
-class EventControllerTest :TestCommons() {
+class EventControllerTest : TestCommons() {
 
 
     @BeforeEach
-    fun injections(){
+    fun injections() {
         beforeEach()
         registerAndLogin()
     }
@@ -30,14 +30,26 @@ class EventControllerTest :TestCommons() {
     }
 
     @Test
-    fun should_allow_to_post_event_as_logged_user()
-    {
+    fun should_return_event_for_user() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/event/user/1").header("Authorization", token))
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").isNotEmpty)
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].userId").isNotEmpty)
+            .andExpect(MockMvcResultMatchers.jsonPath("$[0].puzzleType").isNotEmpty)
+            .andExpect(MockMvcResultMatchers.status().isOk)
+    }
+
+    @Test
+    fun should_allow_to_post_event_as_logged_user() {
         createEvent("1", "FIVE_BY_FIVE ", token).andExpect(MockMvcResultMatchers.status().isCreated)
     }
 
     @Test
     fun should_not_allow_posting_event_if_not_logged_in() {
-        createEvent("1", "FIVE_BY_FIVE ", "not-existing-token").andExpect(MockMvcResultMatchers.status().is4xxClientError)
+        createEvent(
+            "1",
+            "FIVE_BY_FIVE ",
+            "not-existing-token"
+        ).andExpect(MockMvcResultMatchers.status().is4xxClientError)
     }
 
 }
