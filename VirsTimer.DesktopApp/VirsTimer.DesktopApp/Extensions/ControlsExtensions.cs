afbc92c04b68b791d17eeaf0ceff4196e0cfe3c4
Bartosz Hejduk <barhej@st.amu.edu.pl>
2021-11-08 13:08:39 +0100

    Added focusing session textbox while renaming session in SessionChangeView

---
+++ VirsTimer.DesktopApp/VirsTimer.DesktopApp/Extensions/ControlsExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia.VisualTree;
+
+namespace VirsTimer.DesktopApp.Extensions
+{
+    public static class ControlsExtensions
+    {
+        public static Related FindRelatedControl<Control, Parent, Related>(
+            this Control control,
+            Action<Related>? onContinuation = null)
+            where Control : Avalonia.Controls.Control
+            where Parent : Avalonia.Controls.Control
+            where Related : Avalonia.Controls.Control
+        {
+            var parent = control.FindAncestorOfType<Parent>();
+            var related = parent.FindDescendantOfType<Related>();
+            onContinuation?.Invoke(related);
+
+            return related;
+        }
+    }
+}
