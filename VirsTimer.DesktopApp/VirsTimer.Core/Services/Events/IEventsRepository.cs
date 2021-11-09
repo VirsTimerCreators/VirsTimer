445391c67065e3399fd18c2bc8ff5da9ecb5f842
Stefan Chmielnik <stechm@st.amu.edu.pl>
2021-11-09 21:45:21 +0100

    added missing crud method in ieventsrepository interface

--- VirsTimer.DesktopApp/VirsTimer.Core/Services/Events/IEventsRepository.cs
+++ VirsTimer.DesktopApp/VirsTimer.Core/Services/Events/IEventsRepository.cs
@@ -13,5 +13,20 @@ namespace VirsTimer.Core.Services.Events
         /// Gets solves from repository. 
         /// </summary>
         Task<RepositoryResponse<IReadOnlyList<Event>>> GetEventsAsync();
+
+        /// <summary>
+        /// Adds event to repository.
+        /// </summary>
+        Task<RepositoryResponse<Event>> AddEventAsync(Event @event);
+
+        /// <summary>
+        /// Update event in repository.
+        /// </summary>
+        Task<RepositoryResponse> UpdateEventAsync(Event @event);
+
+        /// <summary>
+        /// Deletes event from repository.
+        /// </summary>
+        Task<RepositoryResponse> DeleteEventAsync(Event @event);
     }
 }
