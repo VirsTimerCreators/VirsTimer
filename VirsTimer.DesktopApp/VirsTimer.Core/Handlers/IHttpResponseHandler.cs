6b4cbc41359b12c534ad7356a93b87bc13fb85df
Bartosz Hejduk <barhej@st.amu.edu.pl>
2021-11-10 18:12:26 +0100

    Added summary in IHttpResponseHandler

--- VirsTimer.DesktopApp/VirsTimer.Core/Handlers/IHttpResponseHandler.cs
+++ VirsTimer.DesktopApp/VirsTimer.Core/Handlers/IHttpResponseHandler.cs
@@ -5,9 +5,19 @@ using VirsTimer.Core.Models.Responses;
 
 namespace VirsTimer.Core.Handlers
 {
+    /// <summary>
+    /// Handles getting http responses.
+    /// </summary>
     public interface IHttpResponseHandler
     {
+        /// <summary>
+        /// Calling <paramref name="responseFunc"/> and handles <see cref="HttpResponseMessage"/> wrapping response in <see cref="RepositoryResponse"/>.
+        /// </summary>
         Task<RepositoryResponse> HandleAsync(Func<Task<HttpResponseMessage>> responseFunc);
+
+        /// <summary>
+        /// Calling <paramref name="responseFunc"/> and handles <see cref="HttpResponseMessage"/> wrapping response in <see cref="RepositoryResponse{T}"/> (repository response with value).
+        /// </summary>
         Task<RepositoryResponse<T>> HandleAsync<T>(Func<Task<HttpResponseMessage>> responseFunc);
     }
 }
