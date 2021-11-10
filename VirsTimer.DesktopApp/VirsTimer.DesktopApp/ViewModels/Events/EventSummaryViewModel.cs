130675a842798b4dde40e8427cb334b59ee635ed
Stefan Chmielnik <stechm@st.amu.edu.pl>
2021-11-10 12:27:07 +0100

    Fixed changing event when current was deleted

--- VirsTimer.DesktopApp/VirsTimer.DesktopApp/ViewModels/Events/EventSummaryViewModel.cs
+++ VirsTimer.DesktopApp/VirsTimer.DesktopApp/ViewModels/Events/EventSummaryViewModel.cs
@@ -1,29 +1,30 @@
 ﻿using Avalonia.Controls;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using System.Collections.Specialized;
 using System.Reactive;
 using System.Threading.Tasks;
 using VirsTimer.Core.Models;
 using VirsTimer.Core.Services.Events;
 using VirsTimer.DesktopApp.Views.Events;
-
+ 
 namespace VirsTimer.DesktopApp.ViewModels.Events
 {
     public class EventSummaryViewModel : ViewModelBase
     {
         private readonly IEventsRepository _eventsRepository;
-
+ 
         [Reactive]
         public Event CurrentEvent { get; set; } = null!;
-
+ 
         public ReactiveCommand<Window, Unit> ChangeEventCommand { get; }
-
+ 
         public EventSummaryViewModel(IEventsRepository eventsRepository)
         {
             _eventsRepository = eventsRepository;
             ChangeEventCommand = ReactiveCommand.CreateFromTask<Window>(ChangeEventAsync);
         }
-
+ 
         public override async Task ConstructAsync()
         {
             IsBusy = true;
@@ -31,7 +32,7 @@ namespace VirsTimer.DesktopApp.ViewModels.Events
             CurrentEvent = repositoryResponse.Value[0];
             IsBusy = false;
         }
-
+ 
         private async Task ChangeEventAsync(Window window)
         {
             var eventChangeViewModel = new EventChangeViewModel(_eventsRepository);
@@ -40,10 +41,26 @@ namespace VirsTimer.DesktopApp.ViewModels.Events
             {
                 DataContext = eventChangeViewModel
             };
-
+ 
+            eventChangeViewModel.Events.CollectionChanged += (_, e) => OnEventDelete(e, eventChangeViewModel.Events[0].Event);
             await dialog.ShowDialog(window);
-            if (eventChangeViewModel.SelectedEvent != null)
-                CurrentEvent = eventChangeViewModel.SelectedEvent;
+            if (eventChangeViewModel.Accepted)
+                CurrentEvent = eventChangeViewModel.SelectedEvent!.Event;
+        }
+ 
+        private void OnEventDelete(NotifyCollectionChangedEventArgs e, Event newEvent)
+        {
+            if (e.Action != NotifyCollectionChangedAction.Remove)
+                return;
+ 
+            foreach (EventViewModel @event in e.OldItems)
+            {
+                if (CurrentEvent.Id == @event.Event.Id)
+                {
+                    CurrentEvent = newEvent;
+                    break;
+                }
+            }
         }
     }
 }
