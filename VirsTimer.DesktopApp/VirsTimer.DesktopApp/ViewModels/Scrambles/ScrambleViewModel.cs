8bed4452ed037f91ead31d6770a3c8421c5cf01e
Bartosz Hejduk <barhej@st.amu.edu.pl>
2021-11-29 15:26:56 +0100

    Added info box about scramble generators

--- VirsTimer.DesktopApp/VirsTimer.DesktopApp/ViewModels/Scrambles/ScrambleViewModel.cs
+++ VirsTimer.DesktopApp/VirsTimer.DesktopApp/ViewModels/Scrambles/ScrambleViewModel.cs
@@ -1,10 +1,15 @@
-﻿using ReactiveUI.Fody.Helpers;
+﻿using Avalonia.Controls;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Threading.Tasks;
 using VirsTimer.Core.Models;
 using VirsTimer.Core.Services;
 using VirsTimer.Core.Services.Scrambles;
+using VirsTimer.DesktopApp.ViewModels.Common;
+using VirsTimer.DesktopApp.Views.Common;
 using VirsTimer.Scrambles;
 
 namespace VirsTimer.DesktopApp.ViewModels.Scrambles
@@ -22,12 +27,19 @@ namespace VirsTimer.DesktopApp.ViewModels.Scrambles
         [Reactive]
         public Scramble CurrentScramble { get; set; } = new Scramble();
 
+        [Reactive]
+        public bool GenerateScrambleInfoVisible { get; set; }
+
+        public ReactiveCommand<Window, Unit> OpenGenerateScrambleInfoCommand { get; }
+
         public ScrambleViewModel(
             IScrambleGenerator? scrambleGenerator = null,
             ICustomScrambleGeneratorsCollector? customScrambleGeneratorsCollector = null)
         {
             _scrambleGenerator = scrambleGenerator ?? Ioc.GetService<IScrambleGenerator>();
             _customScrambleGeneratorsCollector = customScrambleGeneratorsCollector ?? Ioc.GetService<ICustomScrambleGeneratorsCollector>();
+
+            OpenGenerateScrambleInfoCommand = ReactiveCommand.CreateFromTask<Window>(OpenGenerateScrambleInfo);
         }
 
         public override Task ConstructAsync()
@@ -39,6 +51,7 @@ namespace VirsTimer.DesktopApp.ViewModels.Scrambles
         {
             IsBusy = true;
             _currentEvent = newEvent;
+            GenerateScrambleInfoVisible = false;
 
             if (Core.Constants.Events.Predefined.Contains(newEvent.Name))
             {
@@ -46,6 +59,7 @@ namespace VirsTimer.DesktopApp.ViewModels.Scrambles
                 var scrabmles = await _scrambleGenerator.GenerateScrambles(_currentEvent, 10).ConfigureAwait(false);
                 _scrambles = new Queue<Scramble>(scrabmles);
                 await GetNextScrambleAsync();
+
                 IsBusy = false;
                 return;
             }
@@ -53,6 +67,10 @@ namespace VirsTimer.DesktopApp.ViewModels.Scrambles
             _isCustom = true;
             _customScrambleGenerator = _customScrambleGeneratorsCollector.GetCustomScrambleGenerators().FirstOrDefault(x => x.EventName == newEvent.Name);
             await GetNextScrambleAsync();
+
+            if (string.IsNullOrWhiteSpace(CurrentScramble.Value))
+                GenerateScrambleInfoVisible = true;
+
             IsBusy = false;
         }
 
@@ -81,6 +99,32 @@ namespace VirsTimer.DesktopApp.ViewModels.Scrambles
                     _scrambles.Enqueue(scramble);
                 IsBusy = false;
             }
+        }
+
+        public static async Task OpenGenerateScrambleInfo(Window window)
+        {
+            var message = string.Join("\n\n",
+                "1. Stworzyć bibliotekę klas (Class Library) w technologii .NET 6.",
+                "2. Dodać paczkę VirsTimer.Scrambles z witryny nuget.org: \n" + 
+                "https://www.nuget.org/packages/Newtonsoft.Json/ \n" + 
+                "do stworzonej bibliteki.",
+                "3. Zaimplementować interfejs ICustomScrambleGenerator gdzie w właściwości EventName należy umieścić nazwę stworzonej konkurencji w aplikacji.\n" +
+                "Biblioteka może zawierać wiele implemenacjia interfejsu, ale tylko jedną dla danej konkurencji.",
+                "4. Zmienić nazwę pliku .dll zbudowanej biblioteki na ScrambleGenerators.dll.",
+                "5. Umieścić plik ScrambleGenerators.dll w głównym folderze aplikacji VirsTimer.",
+                "6. Zresetować aplikację.");
+            
+            var infoBoxViewModel = new InfoBoxViewModel
+            {
+                Message = message
+            };
+
+            var infoBox = new InfoBox
+            {
+                ViewModel = infoBoxViewModel
+            };
+
+            await infoBox.ShowDialog(window);
         }
     }
 }
