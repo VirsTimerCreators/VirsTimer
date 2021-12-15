b32fbea99229dfa65d9efe527eabdb7c54896ee6
Javorus <40281528+Javorus@users.noreply.github.com>
2021-12-15 19:58:50 +0100

    GET many scrambles

--- VirsTimer.Server/virstimer/src/main/kotlin/pl/virstimer/api/ScrambleController.kt
+++ VirsTimer.Server/virstimer/src/main/kotlin/pl/virstimer/api/ScrambleController.kt
@@ -1,7 +1,10 @@
 package pl.virstimer.api
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import pl.virstimer.domain.DomainScramble
 import pl.virstimer.domain.PuzzleType
@@ -13,9 +16,19 @@ class ScrambleController(
 ) {
     @GetMapping("/scramble/{puzzleType}")
     fun getScramble(
-            @PathVariable puzzleType: PuzzleType
-    ): ScrambleResponse =
-            scrambleService.generateScrambleAndSvg(puzzleType).toScrambleResponse()
+        @PathVariable puzzleType: PuzzleType, @RequestParam(required = false) amount: Int?
+    ): ResponseEntity<MutableList<ScrambleResponse>> {
+        val list = mutableListOf<ScrambleResponse>()
+        if (amount != null) {
+            repeat(amount) {
+                list.add(scrambleService.generateScrambleAndSvg(puzzleType).toScrambleResponse())
+            }
+
+        }
+        else list.add(scrambleService.generateScrambleAndSvg(puzzleType).toScrambleResponse())
+
+        return ResponseEntity(list, HttpStatus.OK)
+    }
 }
 
 data class ScrambleResponse(
